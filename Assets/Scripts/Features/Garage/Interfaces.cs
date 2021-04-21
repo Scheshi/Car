bf8167e3be765597e608c496152b3fda18b640ea
Darari <enjerudar@gmail.com>
2021-04-21 23:20:20 +0700

    Добавление интерфейсов для гаража

---
+++ Assets/Scripts/Features/Garage/Interfaces.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Scripts.Features.Garage
+{
+    public interface IUpgradableCar
+    {
+        float Speed { get; }
+        void Restore();
+    }
+
+    public interface IUpgradableCarHandler
+    {
+        IUpgradableCar UpgradeCar(IUpgradableCar car);
+    }
+}
