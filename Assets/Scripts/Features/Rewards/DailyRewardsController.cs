855a4892fd6960bf901129178a8c35c95f0d67b8
Darari <enjerudar@gmail.com>
2021-05-10 21:35:15 +0700

    Реализация вьюшек для ежедневных наград

---
+++ Assets/Scripts/Features/Rewards/DailyRewardsController.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Controllers;
+using UnityEngine;
+
+namespace Assets.Scripts.Features.Rewards
+{
+    public class DailyRewardsController : BaseController
+    {
+        private string _pathToPrefab;
+        private DailyRewardsView _view;
+
+        public void Init(Transform uiPlace)
+        {
+            _view = LoadView<DailyRewardsView>(_pathToPrefab, uiPlace);
+        }
+    }
+}
--- Assets/Scripts/Features/Rewards/DailyRewardsView.cs
+++ Assets/Scripts/Features/Rewards/DailyRewardsView.cs
@@ -1,9 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using Assets.Scripts.Enums;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Features.Rewards
 {
     public class DailyRewardsView : MonoBehaviour
     {
-        
+        [SerializeField] private Transform _content;
+        [SerializeField] private Button _backToMenuButton;
+
+        public void Init(Action<DailyType, int> onReward, Action onBackToMenu)
+        {
+            _backToMenuButton.onClick.AddListener(onBackToMenu.Invoke);
+            if (_content.childCount > 0)
+            {
+                foreach (var rewardButton in _content.GetComponentsInChildren<RewardSlot>())
+                {
+                    rewardButton.Init(onReward);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _backToMenuButton.onClick.RemoveAllListeners();
+        }
     }
 }
