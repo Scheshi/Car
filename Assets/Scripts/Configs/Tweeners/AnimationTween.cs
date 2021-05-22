29267709f928443300b660a8ca1e17a1b29bf9a5
Darari <enjerudar@gmail.com>
2021-05-23 02:10:46 +0700

    Залил твин, сделал анимацию для валидного вознаграждения

---
+++ Assets/Scripts/Configs/Tweeners/AnimationTween.cs
@@ -0,0 +1,16 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts.Configs.Tweeners
+{
+    [CreateAssetMenu(menuName =  "Configs/Tweeners/AnimationTween")]
+    public class AnimationTween : ScriptableObject
+    {
+        public Ease Ease;
+        public float Duration;
+        public Vector3 MoveValue;
+        public int CountLoops;
+        public float ScaleValue;
+
+    }
+}
