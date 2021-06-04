23494498a1473070dbede3de9d31c03c0eafb51a
Darari <enjerudar@gmail.com>
2021-06-04 22:35:14 +0700

    Залил менеджер нотификаций и сделал тестовую нотификацию

--- Assets/Scripts/Services/GameStarter.cs
+++ Assets/Scripts/Services/GameStarter.cs
@@ -1,6 +1,8 @@
+using System;
 using Assets.Scripts.Controllers;
 using Assets.Scripts.Profile;
 using Assets.Scripts.Services;
+using NotificationSamples;
 using UnityEngine;
 
 
@@ -10,17 +12,21 @@ public class GameStarter : MonoBehaviour
     [SerializeField] private float _speedCar;
     private MainController _controller;
     private PlayerProfile _profile;
+    public static GameNotificationsManager NotificationsManager;
     
     
-    void Start()
+    private void Start()
     {
         new GameObject("Updater").AddComponent<GameUpdater>();
+        NotificationsManager = new GameObject("Notification").AddComponent<GameNotificationsManager>();
+        NotificationsManager.Initialize(new GameNotificationChannel("Test", "Test", "Test"));
         _profile = new PlayerProfile(_speedCar, new UnityAnalytic());
         _controller = new MainController(_profile, _placeUI);
     }
 
     private void OnDestroy()
     {
+        Destroy(NotificationsManager.gameObject);
         _controller.Dispose();
     }
 
